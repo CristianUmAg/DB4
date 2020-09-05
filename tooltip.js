0e27b71552e348c5fff29114b247aaa5580573f6
CristianUmAg <65322158+CristianUmAg@users.noreply.github.com>
2020-09-04 23:07:58 -0500

    Se agregaron unos cambios "scipts" en el modal

--- tooltip.js
+++ tooltip.js
@@ -2,6 +2,26 @@ $(function(){
   $("[data-toggle='tooltip']").tooltip();
   $("[data-toggle='popover']").popover();
   $('.carousel').carousel({
-    interval: 5000
+    interval: 2500
   });
+
+  $('#contacto').on('show.bs.modal', function (e) {
+    console.log('El modal se esta mostrando :))');
+    $('#contactoBtn').removeClass('btn-outline-success');
+    $('#contactoBtn').addClass('btn-dark');
+    $('#contactoBtn').prop('disabled', true);
+  });
+  $('#contacto').on('shown.bs.modal', function (e) {
+    console.log('El modal se mostró :))');
+  });
+  $('#contacto').on('hide.bs.modal', function (e) {
+    console.log('El modal se esta ocultando :))');
+  });
+  $('#contacto').on('hidden.bs.modal', function (e) {
+    console.log('El modal se ocultó :))');
+    $('#contactoBtn').removeClass('btn-dark');
+    $('#contactoBtn').addClass('btn-outline-success');
+    $('#contactoBtn').prop('disabled', false);
+  });
+
 });
